bf6ca0dc6ae9034b2bafff7c37c3b920b3ea6bed
Susie O'Brien <email": null>
2016-07-22 11:55:06 -0500

    All associatons work

--- Waitr/db/migrate/20160722152450_create_prizes.rb
+++ Waitr/db/migrate/20160722152450_create_prizes.rb
@@ -1,7 +1,7 @@
 class CreatePrizes < ActiveRecord::Migration[5.0]
   def change
     create_table :prizes do |t|
-      t.references :restaurant
+      t.integer :restaurant_id
       t.integer :points_needed, null: false
       t.string :prize_name, null: false
       t.boolean :active, default: false
--- Waitr/db/migrate/20160722152613_create_rounds.rb
+++ Waitr/db/migrate/20160722152613_create_rounds.rb
@@ -1,9 +1,9 @@
 class CreateRounds < ActiveRecord::Migration[5.0]
   def change
     create_table :rounds do |t|
-      t.references :quiz
+      t.integer :quiz_id
       t.integer :party_one_id, null: false
-      t.ingeger :party_two_id, default: 0
+      t.integer :party_two_id, default: 0
       t.integer :party_one_score, default: 0
       t.integer :party_two_score, default: 0
 
