10d9c116c388a1833f654c2579425680704d2364
Mark Cassidy <mcassid3@gmail.com>
2016-07-22 22:44:27 -0500

    react timer working need to fix clearinterval

--- Waitr/app/assets/javascripts/components/QuestionTimer.js.jsx
+++ Waitr/app/assets/javascripts/components/QuestionTimer.js.jsx
@@ -1,17 +1,21 @@
 var QuestionTimer = React.createClass({
   getInitialState: function(){
-    return {timer: 15};
+    return {timer: 5};
   },
   componentDidMount: function(){
     this.startTimer();
   },
+  componentWillUnmount: function(){
+    clearInterval(interval);
+  },
 
   startTimer: function() {
-    setInterval(function(){
-      if (this.state.timer === 0) {
-        $.get()
+    var interval = setInterval(function(){
+       console.log("RUNNING")
+      if (this.state.timer <= 0) {
         return;
       }
+
       this.decreaseTimer()
     }.bind(this), 1000)
   },
@@ -19,7 +23,6 @@ var QuestionTimer = React.createClass({
   decreaseTimer: function(){
     this.setState({timer: this.state.timer - 1})
   },
-
   render: function(){
     console.log(this.state.timer)
     return (
--- Waitr/app/controllers/questions_controller.rb
+++ Waitr/app/controllers/questions_controller.rb
@@ -1,12 +1,8 @@
 class QuestionsController < ApplicationController
   def show
     @quiz = Quiz.find(params[:quiz_id])
-    p @quiz
     @round = Round.find(params[:round_id])
-    p "ROUNDNDDDDDDJHAGSDJHSGDJH"
-    p @round
     @question = @quiz.select_question
-    p @question
     @guess = Guess.new(round_id: @round.id, question_id: @question.id)
   end
 end
