cdc9911de7757e7cb8ee7ef3afa31c21d67ec61f
Stephano Hong <stephano.j.hong@gmail.com>
2016-07-22 22:42:33 -0500

    Add login and make root to show profile if logged in and login if not logged in

--- Waitr/app/models/restaurant.rb
+++ Waitr/app/models/restaurant.rb
@@ -8,6 +8,8 @@ class Restaurant < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
+  def 
+
   def waiting_list
     waiting_list = []
     active_parties = self.parties.where(in_queue: true)
