6a6de19c29d22292b121a6a692969c786535dc44
Mark Cassidy <mcassid3@gmail.com>
2016-07-22 21:53:23 -0500

    questions queue guesses get saved

--- Waitr/app/models/quiz.rb
+++ Waitr/app/models/quiz.rb
@@ -2,7 +2,13 @@ class Quiz < ApplicationRecord
   has_many :questions
 
   validates :seconds_per_question, presence: true
-
+  def select_question
+    # need to refine to select only questions that do not have prior associated guesses
+    questions = self.questions
+    length = questions.length
+    randnum = rand(length)
+    questions[randnum]
+  end
 
 
 end
