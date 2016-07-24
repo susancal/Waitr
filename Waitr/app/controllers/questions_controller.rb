fa054cb02d4118c6ae9cb1ece343617b8bf05690
Mark Cassidy <mcassid3@gmail.com>
2016-07-23 20:31:38 -0500

    basic score counter implemented - not incrementing yet though

--- Waitr/app/controllers/questions_controller.rb
+++ Waitr/app/controllers/questions_controller.rb
@@ -1,10 +1,3 @@
 class QuestionsController < ApplicationController
-  def show
-    @quiz = Quiz.find(params[:quiz_id])
-    @round_id = params[:round_id]
-    # @question = @quiz.select_question
-    # @guess = Guess.new(round_id: @round.id, question_id: @question.id)
-    @quiz_questions = @quiz.questions
-    @party_id = 2
-  end
+
 end
--- Waitr/app/controllers/rounds_controller.rb
+++ Waitr/app/controllers/rounds_controller.rb
@@ -11,11 +11,19 @@ class RoundsController < ApplicationController
 
   def create
     @player1 = Party.find(params[:player_one_id])
-    @round = Round.create(party_one_id: params[:player_one_id])
-    p @round
+    @round = Round.create!(party_one_id: params[:player_one_id])
+    if @round
+      p "SHITTTTTTT"
+    end
     render :waiting
   end
 
+  def show
+    @quiz = Quiz.find(params[:quiz_id])
+    @round = Round.find(params[:id])
+    @quiz_questions = @quiz.questions
+    @party_id = 1
+  end
 
 
   def summary
