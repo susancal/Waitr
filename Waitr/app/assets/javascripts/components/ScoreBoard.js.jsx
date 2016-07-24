0d811ff7c29e29e84fac9a7ca082ad941913edc3
Mark Cassidy <mcassid3@gmail.com>
2016-07-24 17:29:38 -0500

    guess button disabled upon save - score increments - correct and incorrect displays - pauses between questions - css improvments for mobile

--- Waitr/app/assets/javascripts/components/ScoreBoard.js.jsx
+++ Waitr/app/assets/javascripts/components/ScoreBoard.js.jsx
@@ -1,13 +1,8 @@
 var ScoreBoard = React.createClass({
-  getInitialState: function() {
-      return {current_score: 0};
-  },
-
   render: function(){
     return (
       <div>
-        <h1>Group 1 Score</h1>
-        <h1> {this.state.current_score} </h1>
+        <h3 className="score-board">Round Points: {this.props.playerOneScore}</h3>
       </div>
     )
   }
