e2b554ec8400e19d14fc8d237642f7186fc2489d
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-07-27 11:03:01 -0500

    Configure actioncable

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -8,6 +8,6 @@
 (function() {
   this.App || (this.App = {});
 
-  App.cable = ActionCable.createConsumer('wss://waitr-app.herokuapp.com/cable');
+  App.cable = ActionCable.createConsumer();
 
 }).call(this);
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  # Specify the socket URI for actioncable code.
+  config.action_cable.url = "wss://waitr-app.herokuapp.com/cable"
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
