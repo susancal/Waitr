0f9baf11aa9fafd7061b2735dd8655042849dd6b
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-07-27 10:26:06 -0500

    Add heroku config

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -8,6 +8,6 @@
 (function() {
   this.App || (this.App = {});
 
-  App.cable = ActionCable.createConsumer('ws://waitr-app.herokuapp.com/cable');
+  App.cable = ActionCable.createConsumer('wss://waitr-app.herokuapp.com/cable');
 
 }).call(this);
