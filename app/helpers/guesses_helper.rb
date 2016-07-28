7bc4b0cba2075582d6b6dd9aeecf21996ba64940
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-07-28 13:43:11 -0500

    Update the gsub regex statement

--- app/helpers/guesses_helper.rb
+++ app/helpers/guesses_helper.rb
@@ -1,6 +1,7 @@
 module GuessesHelper
 
   def sanitize(answer)
-    return answer.downcase.gsub(/\s+/, '')
+    # return answer.downcase.gsub(/\s+/, '')
+    return answer.downcase.gsub(/<i>|\s+|<\/i>/, '')
   end
 end
