110556c3a71186b01bc157b67d4570f15eeca034
Susie O'Brien <email": null>
2016-07-29 12:08:43 -0500

    Take out current party logout

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,8 @@
 
 
 Restaurant.create!(name: "The Matte Bakery", email:"matt@baker.com", password: "password")
-Restaurant.create!(name: "Susie's diner", email:"susie@susies.com", password: "password")
 
 Party.create!(restaurant_id: 1, name: "Smiths", size: 2, cell: "7738958335", in_queue: true, points_earned: 0)
-Party.create!(restaurant_id: 1, name: "Maxwell", size: 4, cell: "7734562334", in_queue: true, points_earned: 1000)
-Party.create!(restaurant_id: 1, name: "Johnson", size: 8, cell: "7739050234", in_queue: true, points_earned: 2000)
-Party.create!(restaurant_id: 1, name: "Fonzi", size: 2, cell: "7736243333", in_queue: true, points_earned: 500)
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Quiz.create!
