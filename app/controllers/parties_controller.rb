62cd2a1d6e9f68d599cf01dd4777d0811305f5df
Susie O'Brien <email": null>
2016-08-02 20:51:53 -0500

    Add in create parties - commented out too much previously. Phone info is still gone

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -27,25 +27,25 @@ class PartiesController < ApplicationController
   def edit
     party = Party.find(params[:id])
 
-    if party.did_they_win?
-      party.send_prize_email
-    end
+    # if party.did_they_win?
+    #   party.send_prize_email
+    # end
 
     party.in_queue = false
     party.save
     redirect_to root_path
   end
 
-  # def create
-  #   # p request.protocol + request.host
-  #   # p request.base_url
+  def create
+    # p request.protocol + request.host
+    # p request.base_url
 
-  #   party = Party.new(party_params)
-  #   party.restaurant_id = params[:restaurant_id]
+    party = Party.new(party_params)
+    party.restaurant_id = params[:restaurant_id]
 
-  #   if party.save
-  #     # session[:party_id] = party.id
-  #     # session[:party_key] = party.key
+    if party.save
+      # session[:party_id] = party.id
+      # session[:party_key] = party.key
 
 
   #     phone_numbers = ['+', '+', '+', '+']
@@ -67,11 +67,11 @@ class PartiesController < ApplicationController
   #         body: "Thank you for waiting at #{party.restaurant.name}.  Click here to check your wait time and play a game. #{link}"
   #       )
   #     end
-  #     redirect_to root_path
-  #   else
-  #     redirect_to new_restaurant_party_path
-  #   end
-  # end
+      redirect_to root_path
+    else
+      redirect_to new_restaurant_party_path
+    end
+  end
 
   private
     def party_params
